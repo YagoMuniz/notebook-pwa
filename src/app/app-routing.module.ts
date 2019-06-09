456fa14d6fc934c48575d0327c1533e1966d3f15
Joel S. Junior <joelsantosjunior1996@gmail.com>
2019-06-08 23:13:10 -0300

    Adds a login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageComponent } from './page/page.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'page', pathMatch: 'full'},
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
   { path: 'page', component: PageComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,34 @@ import { PageComponent } from './page/page.component';
 
 // Services
 import { ApiService } from './services/api.service';
+import { NgZorroAntdModule, NZ_I18N, pt_BR } from 'ng-zorro-antd';
+import { FormsModule, FormBuilder } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { LoginComponent } from './login/login.component';
+
+registerLocaleData(pt);
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    PageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
-    ApiService
+    ApiService,
+    FormBuilder,
+    { provide: NZ_I18N, useValue: pt_BR }
   ],
   bootstrap: [AppComponent]
 })
