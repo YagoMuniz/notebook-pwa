be0a565ff8a3c9f1104f14967da08827ede78fe1
Joel S. Junior <joelsantosjunior1996@gmail.com>
2019-06-09 13:35:29 -0300

    Finishes the login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ApiService } from "../services/api.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
   validateForm: FormGroup;
 
-  submitForm(): void {
+  async submitForm() {
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[i].markAsDirty();
       this.validateForm.controls[i].updateValueAndValidity();
     }
+
+    if (this.validateForm.invalid) {
+      alert("Formulário inválido");
+      return;
+    }
+
+    try {
+      const response = await this.api.login(
+        this.validateForm.value.userName,
+        this.validateForm.value.password
+      );
+  
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private api: ApiService) {}
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,29 +1,47 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { RegisterModel } from '../register/register.model';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { RegisterModel } from "../register/register.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ApiService {
-
-  private baseUrl = 'https://back-notebook-pwa.herokuapp.com/';
+  private baseUrl = "https://back-notebook-pwa.herokuapp.com/";
   private endpoints = {
-    ACCESS_TOKEN: 'qpnYKkKXDH8126YEOjqMRrrWXrI5f1wV',
-    USERS: 'users'
-  }
+    ACCESS_TOKEN: "qpnYKkKXDH8126YEOjqMRrrWXrI5f1wV",
+    USERS: "users",
+    LOGIN: "auth"
+  };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   register(body: RegisterModel): Promise<any> {
     body.access_token = this.endpoints.ACCESS_TOKEN;
-    return this.http.post(
-      this.baseUrl + this.endpoints.USERS,
-      body,
-      {}
-    ).toPromise();
+    return this.http
+      .post<any>(this.baseUrl + this.endpoints.USERS, body, {})
+      .toPromise();
   }
 
-  login() {}
+  login(username: string, password: string): Promise<any> {
+    const httpHeaders = new HttpHeaders({
+      'Authorization': "Basic " + window.btoa(username + ":" + password)
+    });
+
+    return this.http
+      .post<any>(
+        this.baseUrl + this.endpoints.LOGIN,
+        { 'access_token': this.endpoints.ACCESS_TOKEN },
+        { headers: httpHeaders }
+      )
+      .pipe(map(user => {
+        if (user) {
+          localStorage.setItem("current_auth", user.token);
+          localStorage.setItem("loggedUser", JSON.stringify(user.user));
+        }
+        return user;
+      }))
+      .toPromise();
+  }
 }
