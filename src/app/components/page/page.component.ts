ffa96fad727d2d3de911f7931cec21c7c5e222d0
Yago Muniz <yagomunizns@gmail.com>
2019-06-10 18:08:09 -0300

    Updating project

--- src/app/components/page/page.component.ts
+++ src/app/components/page/page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnChanges, SimpleChanges, ViewChild, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { Notebook } from '../novo-caderno/notebook.model';
 import { NzMessageService } from 'ng-zorro-antd';
@@ -9,67 +9,61 @@ import { NzMessageService } from 'ng-zorro-antd';
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
-export class PageComponent implements OnInit, OnChanges {
+export class PageComponent implements OnInit, OnDestroy{
+  ngOnDestroy(): void {
+    this.notebook = undefined;
+  }
 
   notebook: Notebook;
-  pageToShow: 0;
-  @ViewChild('elementeditable') input; 
+  @ViewChild('elementeditable') input;
 
   constructor(private api: ApiService,
     private message: NzMessageService,
-    private route: ActivatedRoute) { }
+    private router: Router,
+    private route: ActivatedRoute) {
+      router.events.subscribe((val) => {
+        // see also 
+        if (val instanceof NavigationEnd) {
+          this.notebook = undefined;
+        }
+    });
+    }
 
   ngOnInit() {
     this.route.params.subscribe(async data => {
+
       const id = this.message.loading('Carregando Notebook...', { nzDuration: 0 }).messageId;
       this.notebook = await this.api.loadNotebookById(data['id']);
 
-      if (this.notebook.pages.length === 0) {
-        for (let i = 0; i < this.notebook.number_of_pages; i++)
-          this.notebook.pages.push(' ');
-          try {
-            this.notebook = await this.api.editNotbookById(this.notebook.id, this.notebook);
-            console.log('this.notebook: ', this.notebook);
-          } catch (error) {
-            console.log(error);
-          }
-
-      }
+      await this.addDefaultPages();
 
       this.message.remove(id);
     });
-
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log(changes)
+  async addDefaultPages() {
+    if (this.notebook.pages.length === 0) {
+      for (let i = 0; i < this.notebook.number_of_pages; i++)
+        this.notebook.pages.push('>');
+        try {
+          this.notebook = await this.api.editNotbookById(this.notebook.id || this.notebook._id, this.notebook);
+          console.log('this.notebook: ', this.notebook);
+        } catch (error) {
+          console.log(error);
+        }
+
+    }
   }
 
   async onChange(input) {
     this.notebook.pages[0] = input;
     try {
-      this.notebook = await this.api.editNotbookById(this.notebook.id, this.notebook);
+      this.notebook = await this.api.editNotbookById(this.notebook.id || this.notebook._id, this.notebook);
       console.log('Updated', this.notebook);
     } catch (error) {
       console.log(error);
     }
 
-  }
-
-  onNext() {
-    if (this.pageToShow < this.notebook.number_of_pages) {
-      this.pageToShow++;
-    }
-
-    console.log(this.notebook.pages)
-  }
-  
-  onPrev() {
-    if (this.pageToShow > 0) {
-      this.pageToShow--;
-    }
-
-    console.log(this.notebook.pages)
   }
 
 }
