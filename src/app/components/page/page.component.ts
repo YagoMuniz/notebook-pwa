1715fe2915392dd004f68fde930b684a05db647c
Joel Santos Junior <joel@upplify.com>
2019-06-10 15:04:29 -0300

    Show page content

--- src/app/components/page/page.component.ts
+++ src/app/components/page/page.component.ts
@@ -11,20 +11,27 @@ import { NzMessageService } from 'ng-zorro-antd';
 })
 export class PageComponent implements OnInit, OnChanges {
 
-  pageContent:string;
   notebook: Notebook;
-  
+  pageToShow: 0;
+
   constructor(private api: ApiService,
-              private message: NzMessageService,
-              private route: ActivatedRoute) { }
+    private message: NzMessageService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe(async data => {
       const id = this.message.loading('Carregando Notebook...', { nzDuration: 0 }).messageId;
       this.notebook = await this.api.loadNotebookById(data['id']);
+
+      if (this.notebook.pages.length === 0) {
+        for (let i = 0; i < this.notebook.number_of_pages; i++)
+          this.notebook.pages.push(' ');
+      }
+
+      console.log('this.notebook: ', this.notebook);
       this.message.remove(id);
-    })
-    this.pageContent = '';
+    });
+
   }
 
   ngOnChanges(changes: SimpleChanges) {
