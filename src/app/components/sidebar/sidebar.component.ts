cbd48032c3070f0eda36444bcda884c21b804360
Joel S. Junior <joelsantosjunior1996@gmail.com>
2019-06-10 00:47:01 -0300

    fix messages

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "src/app/services/api.service";
+import { Router } from "@angular/router";
+import { NzMessageService } from "ng-zorro-antd";
+import { Notebook } from "../novo-caderno/notebook.model";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent implements OnInit {
-
   notebooks: any;
-  constructor(private api: ApiService,
-              private router: Router) { }
+  constructor(
+    private api: ApiService,
+    private message: NzMessageService,
+    private router: Router
+  ) {}
 
   async ngOnInit() {
     await this.loadNotebooks();
@@ -19,16 +23,23 @@ export class SidebarComponent implements OnInit {
 
   async logout() {
     await this.api.logout();
-    this.router.navigateByUrl('login');
+    this.router.navigateByUrl("login");
   }
 
   async loadNotebooks() {
+    const id = this.message.loading("Carregando Notebooks...", {
+      nzDuration: 0
+    }).messageId;
     try {
       this.notebooks = await this.api.loadNotebooks();
-      console.log(this.notebooks)
-    } catch (error) {
 
+      this.message.remove(id);
+    } catch (error) {
+      this.message.remove(id);
     }
   }
 
+  openNotebook(notebook: Notebook) {
+    // this.router.navigateByUrl('', { queryParams: notebook })
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ApiService } from "../services/api.service";
 import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: "app-login",
@@ -22,20 +23,26 @@ export class LoginComponent implements OnInit {
       return;
     }
 
+    const id = this.message.loading('Carregando...', { nzDuration: 0 }).messageId;
     try {
+
       const response = await this.api.login(
         this.validateForm.value.userName,
         this.validateForm.value.password
       );
   
+      this.message.remove(id);
+      
       this.router.navigateByUrl('home');
     } catch (error) {
+      this.message.remove(id);
       console.log(error);
     }
   }
 
   constructor(private fb: FormBuilder,
               private router: Router,
+              private message: NzMessageService,
               private api: ApiService) {}
 
   ngOnInit(): void {
